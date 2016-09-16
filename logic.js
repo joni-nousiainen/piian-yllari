23be86ffb0aedc8298e8bd19684c319bc92a37d5
Joni Nousiainen <developer@joninousiainen.info>
2016-09-17 00:48:38 +0300

    First commit.

---
+++ logic.js
@@ -0,0 +1,52 @@
+function checkPart2() {
+    if ($('#Liisa:checked').length) {
+        openPart('p3');
+    }
+    else {
+        displayTryAgain();
+    }
+}
+
+function checkPart3() {
+    if ($('#Herttakuningatar:checked').length) {
+        openPart('p4');
+    }
+    else {
+        displayTryAgain();
+    }
+}
+
+function checkPart4() {
+    if ($('#mika-tekee-isoksi').val().toLowerCase().indexOf('keks') > -1) {
+        openPart('p5');
+    }
+    else {
+        displayTryAgain();
+        $('#vinkki').show(1000);
+    }
+    return false;
+}
+
+function checkPart5() {
+    if ($('#koodi').val().indexOf('1986') > -1) {
+        openPart('p6');
+    }
+    else {
+        displayTryAgain();
+    }
+    return false;
+}
+
+function openPart(id) {
+    $('.part').hide();
+    $('#' + id).show();
+}
+
+function displayTryAgain() {
+    alert('Väärin meni, yritä uudestaan.');
+
+}
+
+$(document).ready(function() {
+    openPart('p6');
+});
